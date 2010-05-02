96303939cde168b82298d4a006119a85cc165713
Antoine Levitt <antoine.levitt@gmail.com>
2010-05-02 23:48:22 +0100

    Xlib->libwnck

--- prod.c
+++ prod.c
@@ -1,5 +1,4 @@
-// needs libxss-dev and libx11-dev
-//compile with -lX11 -lXss
+//gcc prod.c -o prod -lX11 -lXss -Wall -Wextra $(pkg-config --cflags --libs libwnck-1.0)
 #include <X11/Xutil.h>
 #include <X11/Xlib.h>
 #include <stdio.h>
@@ -7,6 +6,9 @@
 #include <X11/extensions/scrnsaver.h>
 #include <time.h>
 #include <unistd.h>
+#define WNCK_I_KNOW_THIS_IS_UNSTABLE
+#include <libwnck/libwnck.h>
+
 
 Display *display;
 int error()
@@ -28,42 +30,42 @@ int getIdleTime () {
         XScreenSaverQueryInfo(display, RootWindow(display,screen), mit_info);
         idle_time = (mit_info->idle);
         XFree(mit_info);
-        XCloseDisplay(display); 
+        XCloseDisplay(display);
         return idle_time;
 }
-char *getWindowName(Window win)
+
+void window_change_callback(WnckScreen *screen, WnckWindow *prev_window, gpointer user_data)
 {
-	XTextProperty text_prop;
-	if(!XGetWMName(display, win, &text_prop))
-	{
-		// go up
-		Window rroot, rparent, *rchildren;
-		unsigned int rnum;
-		XQueryTree(display, win, &rroot, &rparent, &rchildren, &rnum);
-		char *result = getWindowName(rparent);
-		XFree(rchildren);
-		return result;
+	// unfortunately, due to some weird bug, we can't always assume that the windows won't be NULL
+	printf("Event called\n");
+	wnck_screen_force_update(screen);
+	WnckWindow *active_window = wnck_screen_get_active_window(screen);
+	if(active_window && prev_window) {
+		printf("Leaving %s for %s\n", wnck_window_get_name(prev_window), wnck_window_get_name(active_window));
 	}
 	else
 	{
-		return (char *)text_prop.value;
+		printf("Fail. Active %d, prev %d\n", active_window, prev_window);
 	}
 }
 char *getFocusedWindowName()
 {
-	Window focus;
-	int revert;
-
-	XGetInputFocus(display, &focus, &revert);
-	return getWindowName(focus);
+	WnckScreen *screen = wnck_screen_get_default();
+	wnck_screen_force_update(screen);
+	WnckWindow *window = wnck_screen_get_active_window(screen);
+	return wnck_window_get_name(window);
 }
 
 
-int main()
+int main(int argc, char *argv[])
 {
-	display = XOpenDisplay(NULL);
+	gtk_init(&argc, &argv);
 
-	printf("%s\n", getFocusedWindowName());
 	printf("%d\n", getIdleTime());
+
+	WnckScreen *screen = wnck_screen_get_default();
+	g_signal_connect (screen, "active-window-changed", G_CALLBACK (window_change_callback), NULL);
+	gtk_main ();
+
 	return 0;
 }
