bb0e584fb2cecd8d13b4e0fe26bb4117de62e22d
Antoine Levitt <antoine.levitt@gmail.com>
2010-05-03 20:10:53 +0100

    Contract names of some windows

--- prod.c
+++ prod.c
@@ -75,26 +75,50 @@ void print_table()
 
 	qsort(pair_arr, N, sizeof(struct pair), compare_pairs);
 
+	printf("--------------------------------------------------------------------------------\n");
 	for(i = 0;i<N;i++)
 	{
 		printf("%4d %s\n", pair_arr[i].value, pair_arr[i].key);
 	}
+	printf("\n");
 
 	free(pair_arr);
+}
+
+//if the name of the window contains one of these strings, it is
+//replaced by it. Avoids having a lot of entries for similar activities
+char *contract_names_list[] = {
+	"Slashdot",
+	"Google Reader",
+	"Facebook",
+	".m - Emacs",
+	".tex - Emacs"
+};
+//return a new string, post-processed
+char *post_process_names(char *win_name)
+{
+	int i;
+	int N = sizeof(contract_names_list)/sizeof(*contract_names_list);
+	for(i = 0; i < N;i++) {
+		if(strstr(win_name, contract_names_list[i])) {
+			return strdup(contract_names_list[i]);
+		}
+	}
+	return strdup(win_name);
 }
 
 void notice_window_is_inactive()
 {
 	assert(current_window);
-	printf("You spent %lds on %s\n", time(NULL) - current_window_since, current_window_name);
-	gpointer res = g_hash_table_lookup(table, current_window_name);
+	char *name = post_process_names(current_window_name);
+	//printf("You spent %lds on %s\n", time(NULL) - current_window_since, current_window_name);
+	
+	gpointer res = g_hash_table_lookup(table, name);
 	int new_score = (res ? GPOINTER_TO_INT(res) : 0) + time(NULL) - current_window_since;
+	g_hash_table_insert(table, name, GINT_TO_POINTER(new_score));
+	// if there was already a key, we did the copy for nothing
 	if(res)
-		g_hash_table_insert(table, current_window_name, GINT_TO_POINTER(new_score));
-	else{
-		// have to duplicate, otherwise it will get free'd
-		g_hash_table_insert(table, strdup(current_window_name), GINT_TO_POINTER(new_score));
-	}
+		free(name);
 	print_table();
 }
 
