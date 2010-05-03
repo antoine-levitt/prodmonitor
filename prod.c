64b8bd249bbabcd08b8361ae1b364b630fbeb062
Antoine Levitt <antoine.levitt@gmail.com>
2010-05-03 17:50:36 +0100

    Fixes, listen to name changes

--- prod.c
+++ prod.c
@@ -8,9 +8,14 @@
 #include <unistd.h>
 #define WNCK_I_KNOW_THIS_IS_UNSTABLE
 #include <libwnck/libwnck.h>
+#include <assert.h>
 
 
 Display *display;
+WnckWindow *current_window = NULL;
+char *current_window_name = NULL; //this is a COPY, it has to be created with strdup and free'd
+time_t current_window_since;
+gulong handler_id; // id of the handler for window name change events
 int error()
 {
 	printf("error\n");
@@ -34,18 +39,59 @@ int getIdleTime () {
         return idle_time;
 }
 
+void notice_window_is_inactive()
+{
+	assert(current_window);
+	printf("You spent %ds on %s\n", time(NULL) - current_window_since, current_window_name);
+}
+
+void window_name_change_callback(WnckWindow *win, gpointer user_data)
+{
+	//printf("Name change callback, %d\n", win);
+	if(win) {
+		assert(win == current_window);
+		notice_window_is_inactive();
+		free(current_window_name);
+		current_window_name = strdup(wnck_window_get_name(win));
+		time(&current_window_since);
+	}
+}
 void window_change_callback(WnckScreen *screen, WnckWindow *prev_window, gpointer user_data)
 {
-	// unfortunately, due to some weird bug, we can't always assume that the windows won't be NULL
-	printf("Event called\n");
 	wnck_screen_force_update(screen);
 	WnckWindow *active_window = wnck_screen_get_active_window(screen);
-	if(active_window && prev_window) {
-		printf("Leaving %s for %s\n", wnck_window_get_name(prev_window), wnck_window_get_name(active_window));
+	
+	/* // unfortunately, due to some weird bug, we can't always assume that the windows won't be NULL */
+	/* if(active_window && prev_window) { */
+	/* 	printf("Leaving %s for %s\n", wnck_window_get_name(prev_window), wnck_window_get_name(active_window)); */
+	/* } */
+	/* else */
+	/* { */
+	/* 	printf("Active %d, prev %d\n", active_window, prev_window); */
+	/* } */
+
+	// assume that the event saying that prev_window is no longer
+	// active comes BEFORE the one saying that active_window is active
+
+	// leaving a window
+	if(prev_window) {
+		assert(current_window == prev_window);
+		g_signal_handler_disconnect(current_window, handler_id);
+		handler_id = 0;
+		notice_window_is_inactive();
+		current_window = NULL;
+		free(current_window_name);
+		current_window_name = NULL;
 	}
-	else
-	{
-		printf("Fail. Active %d, prev %d\n", active_window, prev_window);
+	
+	// entering a window
+	if(active_window) {
+		current_window = active_window;
+		current_window_name = strdup(wnck_window_get_name(current_window));
+		time(&current_window_since);
+		//register for name changes
+		assert(!handler_id);
+		handler_id = g_signal_connect(active_window, "name-changed", G_CALLBACK (window_name_change_callback), NULL);
 	}
 }
 char *getFocusedWindowName()
